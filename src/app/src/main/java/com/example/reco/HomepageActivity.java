db1c1c591ffd09ca5b21ab79c18acb0f91263fe3
MohamedBengezi <bengezimohamed@gmail.com>
2018-02-02 13:20:14 -0500

    Adding an 'Upload Image' button

--- src/app/src/main/java/com/example/reco/HomepageActivity.java
+++ src/app/src/main/java/com/example/reco/HomepageActivity.java
@@ -2,6 +2,7 @@ package com.example.reco;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        ImageButton loginButton = (ImageButton) findViewById(R.id.upload_button);
+
     }
 }
